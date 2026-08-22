9e5e11b934908f1b0730cc999458421d4ea341c1
naufalf164 <naufalfauzi164@gmail.com>
2026-08-22 18:18:01 +0700

    Create clients.js

---
+++ clients.js
@@ -0,0 +1,36 @@
+// clients.js
+const clientsData = [
+  {
+    id: "nesa-andri",
+    name: "Nesa & Andri",
+    date: "18 Agustus 2026",
+    venue: "Rajamandala",
+    packageKey: "eternal",
+    packageName: "Eternal Package (± 8 Jam)",
+    isHighlight: true, // Tambahkan opsi ini jika ingin memilih highlight tertentu
+    cover: "https://lh3.googleusercontent.com/d/10BZE_RaRKwoRS9SGBjABCzUefzsxoP31",
+    editedVideos: [
+      { url: "https://drive.google.com/file/d/1WjNqOG3YRle1VzvY7NwzpQ_cbJ3i6ofT/preview" }
+    ],
+    randomPhotos: [
+      { url: "https://lh3.googleusercontent.com/d/1ZiFgREz2cE2l8pSEUlb1VRlcNfijBcw2" }
+    ]
+  },
+  {
+    id: "velin-devan",
+    name: "Velin & Devan",
+    date: "03 Juni 2026",
+    venue: "Gedung Balai Pertemuan Farida, Cianjur",
+    packageKey: "vow",
+    packageName: "Vow Package (Akad ± 4 Jam)",
+    isHighlight: true,
+    cover: "https://lh3.googleusercontent.com/d/1Hp1AhwShB7BpmzD73bZP-AKfmZNpxo8f",
+    editedVideos: [
+      { url: "https://drive.google.com/file/d/1PvFpXSK1FTY8VDsE8rC8PEH4j2NATBLY/preview" }
+    ],
+    randomPhotos: [
+      { url: "https://lh3.googleusercontent.com/d/1Hp1AhwShB7BpmzD73bZP-AKfmZNpxo8f" }
+    ]
+  }
+  // Tambahkan klien baru lainnya di sini...
+];
